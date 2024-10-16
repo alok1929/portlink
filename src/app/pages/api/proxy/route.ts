6182e0d0c52ac2ecbc4f5df6988f97f51aba51b7
alokie <alokie@pop-os.localdomain>
2024-10-16 12:57:32 +0530

    updated proxy url

--- src/app/pages/api/proxy/route.ts
+++ src/app/pages/api/proxy/route.ts
@@ -6,21 +6,25 @@ export async function GET(request: Request) {
   const url = new URL(request.url)
   const targetUrl = `https://portlinkpy.vercel.app${url.pathname}${url.search}`
 
-  return await fetch(targetUrl, {
+  const response = await fetch(targetUrl, {
     headers: request.headers,
     method: request.method,
   })
+
+  return NextResponse.json(await response.json())
 }
 
 export async function POST(request: Request) {
   const url = new URL(request.url)
   const targetUrl = `https://portlinkpy.vercel.app${url.pathname}${url.search}`
 
-  return await fetch(targetUrl, {
+  const response = await fetch(targetUrl, {
     headers: request.headers,
     method: request.method,
     body: request.body,
   })
+
+  return NextResponse.json(await response.json())
 }
 
 // Add similar functions for other HTTP methods you need (PUT, DELETE, etc.)
